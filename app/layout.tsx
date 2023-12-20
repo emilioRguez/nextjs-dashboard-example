cff9a976a725061a82eec0055687a10b39f3193b
Emilio Rodriguez <erdguezh@gmail.com>
2023-12-20 13:19:08 -0500

    Authentication added - Course ended

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,18 @@ import '@/app/ui/global.css';
 import { inter } from '@/app/ui/fonts';
 // Se agrago aqui para que carge las fonts del proyecto.
 
+// Se agrago informacion para la Metadata del proyecto.
+import { Metadata } from 'next';
+ 
+export const metadata: Metadata = {
+  title: {
+    template: '%s | Acme Dashboard',
+    default: 'Acme Dashboard',
+  },
+  description: 'The official Next.js Learn Dashboard built with App Router.',
+  metadataBase: new URL('https://next-learn-dashboard.vercel.sh'),
+};
+
 export default function RootLayout({
   children,
 }: {
